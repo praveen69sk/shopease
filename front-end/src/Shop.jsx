9ab7acb377c54afc340fb96e8ca44d7a9c35909e
praveen69sk <praveen69sk@gmail.com>
2025-04-25 14:57:28 +0530

    Added Navigation bar

---
+++ front-end/src/Shop.jsx
@@ -0,0 +1,12 @@
+import Navigation from './components/Navigation/Navigation';
+import './Shop.css';
+
+function Shop() {
+  return (
+    <div className="Shop">
+      <Navigation />
+    </div>
+  );
+}
+
+export default Shop;
