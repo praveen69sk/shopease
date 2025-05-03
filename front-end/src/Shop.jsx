0e7a4b0bba2762451148804f3eafc54c794f5f37
praveen69sk <praveen69sk@gmail.com>
2025-05-03 16:53:02 +0530

    committing changes

--- front-end/src/Shop.jsx
+++ front-end/src/Shop.jsx
@@ -1,10 +1,14 @@
+import HeroSection from './components/HeroSection/HeroSection';
 import Navigation from './components/Navigation/Navigation';
+import NewArrivals from './components/Sections/NewArrivals';
 import './Shop.css';
 
 function Shop() {
   return (
     <div className="Shop">
       <Navigation />
+      <HeroSection />
+      <NewArrivals />
     </div>
   );
 }
